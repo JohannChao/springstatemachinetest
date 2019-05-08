43675c82021d24ef26298494c423cd65007a8a4a
yhZhao <yhzhaohy@163.com>
2019-05-08 18:19:29 +0800

    statemachine detail

--- src/main/java/com/antsix/Application.java
+++ src/main/java/com/antsix/Application.java
@@ -13,9 +13,22 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class, args);
     }
 
-    @Autowired
+    /**
+     * Could not autowire. No beans of XXX type found
+     */
+    @Autowired(required = false)
     private StateMachine<States, Events> stateMachine;
 
+
+    /**
+     * 在run函数中，我们定义了整个流程的处理过程，
+     * 其中start()就是创建这个订单流程，根据之前的定义，该订单会处于待支付状态，
+     * 然后通过调用sendEvent(Events.PAY)执行支付操作，
+     * 最后通过掉用sendEvent(Events.RECEIVE)来完成收货操作
+     *
+     * @param args
+     * @throws Exception
+     */
     @Override
     public void run(String... args) throws Exception {
         stateMachine.start();
--- src/main/java/com/antsix/EventConfig.java
+++ src/main/java/com/antsix/EventConfig.java
@@ -7,6 +7,14 @@ import org.springframework.statemachine.annotation.OnTransitionEnd;
 import org.springframework.statemachine.annotation.OnTransitionStart;
 import org.springframework.statemachine.annotation.WithStateMachine;
 
+/**
+ * 对于状态监听器，Spring StateMachine还提供了优雅的注解配置实现方式，
+ * 所有 StateMachineListener[org.springframework.statemachine.listener.StateMachineListener] 接口中定义的事件都能通过注解的方式来进行配置实现。
+ * 比如，我们可以将之前实现的状态监听器用注解配置来做进一步的简化：
+ *
+ * 下面的代码实现了与快速入门中定义的listener()方法创建的监听器相同的功能，
+ * 但是由于通过注解的方式配置，省去了原来事件监听器中各种if的判断，使得代码显得更为简洁，拥有了更好的可读性。
+ */
 @WithStateMachine
 public class EventConfig {
 
